2609ebe4ed78bbd2958d50b6cdc257c970cf913e
Joseph Harold <josephharold.timbang.dev@gmail.com>
2023-01-06 20:46:51 +0800

    initialize app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Visualizer-App
+
+
+A web application as a requirement for ITD112
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+
+module.exports = {
+	watch: true, 
+	mode: 'development',
+	entry: {
+		bundle: path.join(__dirname, 'src/js/bundle.js')
+	},
+	output: {
+		path: path.resolve(__dirname, 'static' ),
+		filename: '[name].js',	
+		clean: true
+	},
+	module:{
+		rules: [
+			{
+				test: /\.css/i,
+				use: ['style-loader', 'css-loader', 'postcss-loader']
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)/i,
+				type: 'asset/resource'
+			}
+		]
+	}
+}
